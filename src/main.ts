8743e51f184d66a7b4eb95c98a9da7b8a7bf2506
jjanzen93 <132104176+jjanzen93@users.noreply.github.com>
2024-10-15 16:53:20 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import "./style.css";
 
-const APP_NAME = "Sage's App";
+const APP_NAME = "Sage Draw";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.innerHTML = APP_NAME;
+app.append(title);
+
+const canvas = document.createElement("canvas");
+canvas.height = 256;
+canvas.width = 256;
+app.append(canvas);
