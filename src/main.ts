69f69fa42a912a930715cc0a1b3359d88dc3ee81
jjanzen93 <132104176+jjanzen93@users.noreply.github.com>
2024-10-15 17:14:27 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,44 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 
-const title = document.createElement("h1");
-title.innerHTML = APP_NAME;
-app.append(title);
+const title_txt = document.createElement("h1");
+title_txt.innerHTML = APP_NAME;
+app.append(title_txt);
 
 const canvas = document.createElement("canvas");
 canvas.height = 256;
 canvas.width = 256;
 app.append(canvas);
+
+const ctx = canvas.getContext("2d");
+
+const cursor = {active: false, x: 0, y: 0};
+
+canvas.addEventListener("mousedown", (e) => {
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (cursor.active) {
+        ctx?.beginPath();
+        ctx?.moveTo(cursor.x, cursor.y);
+        ctx?.lineTo(e.offsetX, e.offsetY);
+        ctx?.stroke();
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+    }
+});
+
+canvas.addEventListener("mouseup", (e) => {
+    cursor.active = false;
+});
+
+const clear_button = document.createElement("button");
+clear_button.innerHTML = "clear";
+app.append(clear_button);
+
+clear_button.addEventListener("click", () => {
+    ctx?.clearRect(0, 0, canvas.width, canvas.height);
+});
